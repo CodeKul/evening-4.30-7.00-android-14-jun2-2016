a24e2a12adacf71ce4ce9007668ce21c5bcfd312
aniruddha <aniruddha.kudalkar@gmail.com>
2016-06-17 18:23:56 +0530

    onActivityResult and setResult

--- app/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
+++ app/src/main/java/com/codekul/interactivitycommunication/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String KEY_NEXT_NAME = "nextName";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,14 +34,30 @@ public class MainActivity extends AppCompatActivity {
             if(v.getId() == R.id.btnNext) {
 
                 // i want to start new activity
+               createIntentForGettingDataBack();
+            }
+        }
+    }
 
-                //only "this"  will refer to obj of inner class
-                // but we are expecting object of context i.e. outer class
-                // i.e. MainActivity.this
+    private final void createIntentWithData(){
 
-                Intent intent =
-                        new Intent(MainActivity.this,NextActivity.class);
-                startActivity(intent);
+        //only "this"  will refer to obj of inner class
+        // but we are expecting object of context i.e. outer class
+        // i.e. MainActivity.this
+
+        final EditText edtName = (EditText)
+                findViewById(R.id.edtName);
+
+        Intent intent =
+                new Intent(MainActivity.this,NextActivity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_NEXT_NAME,
+                edtName.getText().toString());
+
+        intent.putExtras(bundle);
+
+        startActivity(intent);
 
                 /* NextActivity.class is in memory representaion of
                 class NextActivity
@@ -48,6 +66,42 @@ public class MainActivity extends AppCompatActivity {
                 clsCar.getFields();
 
                 Class clsAnimal = Animal.class;*/
+    }
+
+    private final void createIntentForGettingDataBack(){
+
+        final EditText edtName = (EditText)
+                findViewById(R.id.edtName);
+
+        Intent intent =
+                new Intent(MainActivity.this,NextActivity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_NEXT_NAME,
+                edtName.getText().toString());
+
+        intent.putExtras(bundle);
+
+        startActivityForResult(intent,1234);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1234){
+
+            if(resultCode == RESULT_OK){
+
+                Bundle transporterBundle = data.getExtras();
+
+                String result = transporterBundle
+                        .getString(NextActivity.KEY_TRANSPORTER_DATA);
+
+
+                final EditText edtName =
+                        (EditText) findViewById(R.id.edtName);
+                edtName.setText(result);
             }
         }
     }
